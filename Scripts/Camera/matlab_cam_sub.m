d7b6cb3c3f93e1f0cae19038c408c0666354f2cc
Samuel <75617851+snchz46@users.noreply.github.com>
2025-06-01 17:27:57 +0200

    Update matlab_cam_sub.m

--- Scripts/Camera/matlab_cam_sub.m
+++ Scripts/Camera/matlab_cam_sub.m
@@ -4,17 +4,17 @@ setenv('ROS_DOMAIN_ID','0');
 %% 
 
 nodeSub = ros2node('imageSubscriberNode');
-disp('🟢 ROS 2 Subscriber Node Created');
+disp('ROS 2 Subscriber Node Created');
 
 imageSub = ros2subscriber(nodeSub, ...
     '/camera/image_raw', ...
     'sensor_msgs/Image', ...
     @imageCallback);
-disp('📥 Subscribed to /camera/image_raw');
+disp('Subscribed to /camera/image_raw');
 
 function imageCallback(msg)
     img = rosReadImage(msg);
     figure(1);
     imshow(img);
-    title(['🖼️ Received Image @ ', datestr(now)]);
+    title(['Received Image @ ', datestr(now)]);
 end

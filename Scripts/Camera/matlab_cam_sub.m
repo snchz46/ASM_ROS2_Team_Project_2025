1fdda8a15b8717d1121a54612d4b5ff05bac9612
Samuel <75617851+snchz46@users.noreply.github.com>
2025-06-01 17:27:22 +0200

    Add files via upload

---
+++ Scripts/Camera/matlab_cam_sub.m
@@ -0,0 +1,20 @@
+%% 
+ros2 topic list
+setenv('ROS_DOMAIN_ID','0');
+%% 
+
+nodeSub = ros2node('imageSubscriberNode');
+disp('🟢 ROS 2 Subscriber Node Created');
+
+imageSub = ros2subscriber(nodeSub, ...
+    '/camera/image_raw', ...
+    'sensor_msgs/Image', ...
+    @imageCallback);
+disp('📥 Subscribed to /camera/image_raw');
+
+function imageCallback(msg)
+    img = rosReadImage(msg);
+    figure(1);
+    imshow(img);
+    title(['🖼️ Received Image @ ', datestr(now)]);
+end
